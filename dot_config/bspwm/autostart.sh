8ca95c4113a038a55ba1cd73d5eba0d5d892174e
Possumvibes <90935434+possumvibes@users.noreply.github.com>
2022-12-03 16:40:26 -0700

    updated bspwm autostarting config

--- dot_config/bspwm/autostart.sh
+++ dot_config/bspwm/autostart.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+# variables and paths
+imagepath=$HOME/.local/share/wallpapers/xfce-101-no-logo.png
+
 # screensaver and locker of choice
 xset s 600 &
-xss-lock ~/scripts/locker.sh &
+xss-lock --transfer-sleep-lock -- i3lock -i $imagepath -M -n &
+
+# compositor
+# picom &
 
 # panel
 polybar example &
