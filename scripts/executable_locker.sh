7c2ffa89bb9bed097096ed1b1ec9aa55e59f0c3f
Possumvibes <90935434+possumvibes@users.noreply.github.com>
2022-12-03 14:42:21 -0700

    updated locker.sh after modifying i3lock fork

--- scripts/executable_locker.sh
+++ scripts/executable_locker.sh
@@ -1,17 +1,24 @@
+#!/bin/bash
+# My preferred settings for i3lock-color.
+# settings commented out are default in my branch as of 12/3/22.
+# Someday perhaps i will mod my fork to read from a config file instead!
+
 i3lock \
   -i ~/.local/share/wallpapers/xfce-101-no-logo.png -M \
-  -c 260012  \
-  -e -k -p default  \
-  --ring-color=260012  \
-  --ringwrong-color=a50018  \
-  --inside-color=26001201  \
-  --insidever-color=26001201  \
-  --insidewrong-color=26001201  \
-  --keyhl-color=60012b  \
-  --bshl-color=d11272  \
-  --ring-width=5  \
-  --wrong-text="Incorrect."  \
-  --verif-text="" \
-  --time-str="%H:%M" \
-  --date-str="%A, %d %b" \
-  --indicator 
+#  -c 260012  \
+#  -e \ # ignore empty passwords
+#  -k \ # show clock
+#  -p default  \ # show  pointer
+#  --ring-color=260012  \
+#  --ringwrong-color=a50018  \
+#  --inside-color=26001201  \
+#  --insidever-color=26001201  \
+#  --insidewrong-color=26001201  \
+#  --keyhl-color=60012b  \
+#  --bshl-color=d11272  \
+#  --ring-width=5  \
+#  --wrong-text="Incorrect."  \
+#  --verif-text="" \
+#  --time-str="%H:%M" \
+#  --date-str="%A, %d %b" \
+#  --indicator 
