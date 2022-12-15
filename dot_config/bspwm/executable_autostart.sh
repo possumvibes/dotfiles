3893ae9ae601b593539ff8377e42faad8e12183c
Possumvibes <90935434+possumvibes@users.noreply.github.com>
2022-12-14 19:15:19 -0700

    added feh bg set to autostart.sh

--- dot_config/bspwm/executable_autostart.sh
+++ dot_config/bspwm/executable_autostart.sh
@@ -2,10 +2,15 @@
 
 # variables and paths
 imagepath=$HOME/.local/share/wallpapers/xfce-101-no-logo.png
+wallpaperdir=$HOME/.local/share/wallpapers/current
+
+# background: someday this will be fancy
+feh --bg-scale $wallpaperdir/deep_space_51_nologo.jpg
 
 # screensaver and locker of choice
 xset s 600 &
-xss-lock --transfer-sleep-lock -- i3lock -i $imagepath -M -n &
+# xss-lock --transfer-sleep-lock -- i3lock -i $imagepath -M -n &
+xss-lock --transfer-sleep-lock -- ~/scripts/locker.sh nofork &
 
 # compositor
 # picom &
