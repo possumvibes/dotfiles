df3f7f08a2c75a6c879972d1bb47c1efefec0d3f
Possumvibes <90935434+possumvibes@users.noreply.github.com>
2022-12-20 16:46:51 -0700

    shell shenanigans, again,

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -5,7 +5,8 @@ source ~/.config/fish/env_vars.fish
 source ~/.config/fish/abbreviations.fish
 
 if status is-interactive
-  set fish_greeting      
+  set fish_greeting
+  set fish_autosuggestion_enabled 0
 
   ## Starship Prompt config
   starship init fish | source
