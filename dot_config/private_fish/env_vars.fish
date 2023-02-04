bb0e87ae98f4a8c6fdeea675099b421870bd0385
Possumvibes <90935434+possumvibes@users.noreply.github.com>
2023-02-03 18:15:35 -0700

    (fish) removed duplicate env vars from env_vars

--- dot_config/private_fish/env_vars.fish
+++ dot_config/private_fish/env_vars.fish
@@ -1,9 +1,1 @@
-
-set -xg EDITOR (which nvim 1>/dev/null 2>/dev/null;
-                and echo nvim;)
-
 set -xg FSHDIR  ~/.config/fish
-
-set -xg STARSHIP_CONFIG ~/.config/starship/starship.toml
-
-set -xg WALLPAPER_DIR $HOME/.local/share/wallpapers/current
