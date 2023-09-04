56486b8019a8207fc0551b8a7d42c256e7e91b57
Possumvibes <90935434+possumvibes@users.noreply.github.com>
2023-09-04 15:52:52 -0600

    shell: added bootstrap scripts and improved xdg compliance in the process

--- shell/.config/shell.d/0_env.sh
+++ shell/.config/shell.d/0_env.sh
@@ -2,27 +2,7 @@
 #
 # Common environment for POSIX-compliant shells
 
-### XDG Base Directories
-export XDG_CACHE_HOME="$HOME/.cache"
-export XDG_CONFIG_HOME="$HOME/.config"
-export XDG_DATA_HOME="$HOME/.local/share"
-export XDG_BIN_HOME="$HOME/.local/bin"
-
-export ZDOTDIR=$XDG_CONFIG_HOME/zsh
-
-# Editor
-export EDITOR="kak"
-export VISUAL="kak"
-
-# Frequently Accessed Directories
-export DOTS_DIR="$XDG_DATA_HOME/chezmoi"
-export STOWDIR="~/.dots"
-export NVIMDIR="$XDG_CONFIG_HOME/nvim"
-export KAKDIR="$XDG_CONFIG_HOME/kak"
-export NOTEBOOK_DIR="$HOME/ari-common/ari_yelling"
-
-export STARSHIP_CONFIG=~/.config/starship/starship.toml
-
+### Common Functions
 # Adds a directory to PATH if it exists and is not already present in PATH
 pathadd() {
   if [ -d "$1" ] && [[ ! $PATH =~ (^|:)$1(:|$) ]]; then
@@ -30,7 +10,51 @@ pathadd() {
   fi
 }
 
+# Quick check if an executable is known
+checkexec() {
+    command -v "$1" > /dev/null
+}
+
+### XDG Base Directories
+export XDG_CACHE_HOME="$HOME/.cache"
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_BIN_HOME="$HOME/.local/bin"
+
+export MAN_HOME="$XDG_DATA_HOME/man"
+
+export ZDOTDIR=$XDG_CONFIG_HOME/zsh
+
+# Set the first available editor as default,
+# ordered in increasing levels of pain.
+for editor in kak nvim vim vi micro nano
+do
+    if checkexec $editor
+    then
+        export EDITOR=$editor
+        export VISUAL=$editor
+        break
+    fi
+done
+
+# Frequently Accessed Directories
+export DOTS_DIR="$XDG_DATA_HOME/dotfiles"
+export STOWDIR="~/.dots"
+export NVIMDIR="$XDG_CONFIG_HOME/nvim"
+export KAKDIR="$XDG_CONFIG_HOME/kak"
+export NOTEBOOK_DIR="$HOME/ari-common/ari_yelling"
+
+# XDG COMPLIANCE AAAAAAA
+export STARSHIP_CONFIG=~/.config/starship/starship.toml
+export NETHACKOPTIONS=$XDG_CONFIG_HOME/nethack/nethackrc
+
+export RUSTUP_HOME=$XDG_DATA_HOME/rustup
+export CARGO_HOME=$XDG_DATA_HOME/cargo
+export LESSHISTFILE=$XDG_CACHE_HOME/less.history
+export PASSWORD_STORE_DIR=$XDG_DATA_HOME/password-store
+
+# Update PATH
 pathadd $HOME/bin
 pathadd $HOME/.local/bin
-pathadd $HOME/.cargo/bin
+pathadd $CARGO_HOME/bin
 
