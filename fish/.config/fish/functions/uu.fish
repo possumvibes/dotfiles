a324cfd11e6e52d200e5d2ce9904055867efe467
Possumvibes <90935434+possumvibes@users.noreply.github.com>
2023-10-19 21:51:51 -0600

    fish: updated functions--fixed l, made uu distro-agnostic

--- fish/.config/fish/functions/uu.fish
+++ fish/.config/fish/functions/uu.fish
@@ -1,3 +1,22 @@
-function uu --wraps='sudo dnf upgrade --refresh -y' --description 'alias uu sudo dnf upgrade --refresh -y'
-  sudo dnf upgrade --refresh -y $argv; 
+# ##########################################################################
+# possumvibes dotfiles
+# https://github.com/possumvibes/dotfiles
+# update.fish - a single function for cross-distro Default Package Updates
+# ##########################################################################
+
+if command -q dnf
+    function uu -d 'dnf package update and upgrade'
+        sudo dnf upgrade --refresh -y $argv;
+    end
+    exit
 end
+
+if command -q apt
+    function uu -d 'apt and apt-get package update and upgrade'
+        sudo apt update && sudo apt upgrade -y
+        sudo apt-get update && sudo apt-get upgrade -y
+    end
+    exit
+end
+
+
