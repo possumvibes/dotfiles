734d444d79e5887250a21e7c899f18f5fb2c6315
Possumvibes <90935434+possumvibes@users.noreply.github.com>
2025-05-04 22:51:52 -0600

    Waybar: Add power menu module from EndeavourOs default Sway config

---
+++ wayland/.config/waybar/modules/power_menu.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+SELECTION="$(printf "󰌾 Lock\n󰍃 Log out\n Reboot\n Reboot to UEFI\n󰐥 Shutdown" | fuzzel --dmenu -a top-right -l 6 -w 18 -p "Select an option: ")"
+
+confirm_action() {
+    local action="$1"
+    CONFIRMATION="$(printf "No\nYes" | fuzzel --dmenu -a top-right -l 2 -w 18 -p "$action?")"
+    [[ "$CONFIRMATION" == *"Yes"* ]]
+}
+
+logout(){
+  if [[ $XDG_SESSION_DESKTOP == "niri" ]]; then
+    niri msg action quit
+  else
+    sway exit
+  fi
+}
+
+case $SELECTION in
+    *"󰌾 Lock"*)
+        gtklock;;
+    # *"󰤄 Suspend"*)
+    #     if confirm_action "Suspend"; then
+    #         systemctl suspend
+    #     fi;;
+    *"󰍃 Log out"*)
+        if confirm_action "Log out"; then
+            niri msg action quit
+        fi;;
+    *" Reboot"*)
+        if confirm_action "Reboot"; then
+            systemctl reboot
+        fi;;
+    *" Reboot to UEFI"*)
+        if confirm_action "Reboot to UEFI"; then
+            systemctl reboot --firmware-setup
+        fi;;
+    *"󰐥 Shutdown"*)
+        if confirm_action "Shutdown"; then
+            systemctl poweroff
+        fi;;
+esac
